913c72a77a0275d98b37d63b759809f10bbebfeb
Luis Ascencio <luismiguelascencio@gmail.com>
2020-03-17 12:31:34 -0400

    fx boost

--- assets/Images.js
+++ assets/Images.js
@@ -1,9 +1,10 @@
 export default Images = {
   background: require('./img/cavebg.jpg'),
-  floor: require('./img/spaceMeteors_001.png'),
+  floor: require('./img/spaceBuilding_007.png'),
   pipeCore: require('./img/spaceBuilding_005.png'),
   pipeTop: require('./img/spaceBuilding_011.png'),
   bird: require('./img/spaceShips_001.png'),
   bird2: require('./img/spaceShips_002.png'),
   bird3: require('./img/spaceShips_003.png'),
+  boost: require('./img/spaceEffects_002.png'),
 };
--- components/Bird.js
+++ components/Bird.js
@@ -6,22 +6,36 @@ export default class Bird extends Component {
   render() {
     const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
     const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
-    const x = this.props.body.position.x - width;
-    const y = this.props.body.position.y - height;
+    const x = this.props.body.position.x - width / 2;
+    const y = this.props.body.position.y - height / 2;
 
-    let image = Images['bird'];
+    let image = Images['bird3'];
     return (
-      <Image
-        style={{
-          position: 'absolute',
-          left: x,
-          top: y,
-          width: width,
-          height: height,
-        }}
-        resizeMode="stretch"
-        source={image}
-      />
+      <View>
+        <Image
+          style={{
+            position: 'absolute',
+            left: x,
+            top: y,
+            width: width,
+            height: height,
+          }}
+          resizeMode="stretch"
+          source={image}
+        />
+        <Image
+          style={{
+            position: 'absolute',
+            left: x,
+            top: y + 40,
+            width: width,
+            height: height,
+            transform: [{rotate: '180deg'}],
+          }}
+          resizeMode="stretch"
+          source={Images['boost']}
+        />
+      </View>
     );
   }
 }
